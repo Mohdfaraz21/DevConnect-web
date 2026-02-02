01697f5c3f9aafa8f7eb74484d06d89ff59bf2e0
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-02-02 13:37:43 +0530

    not auth redirected to login page

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  const user = useSelector((store) => store.user);
+  console.log(user);
+
+  return (
+    <div className="navbar bg-base-300">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">DevConnect</Link>
+      </div>
+
+      {user && (
+        <div className="flex-none gap-2">
+          <div className="form-control">Welcome, {user.firstName}</div>
+          <div className="dropdown dropdown-end mx-5 flex">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="user photo" src={user.photoURL} />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm mx-4 dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to="/profile" className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
