bead173d568fb39f89be0e4b1f4d8b1a49c0f5dc
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-02-03 02:53:00 +0530

    added login and logout features

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUser } from "../utils/userSlice";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
-  console.log(user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(BASE_URL + "/logout", {}, { withCredentials: true });
+      dispatch(removeUser());
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <Link to="/" className="btn btn-ghost text-xl">DevConnect</Link>
+        <Link to="/" className="btn btn-ghost text-xl">
+          🧑‍💻DevConnect
+        </Link>
       </div>
 
       {user && (
@@ -39,7 +56,7 @@ const NavBar = () => {
                 <a>Settings</a>
               </li>
               <li>
-                <a>Logout</a>
+                <a onClick={handleLogout}>Logout</a>
               </li>
             </ul>
           </div>
