c58d78d73d10b4640720fbd7f59a4a2bee1a3a2a
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-02-03 12:37:31 +0530

    working with user feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import { addFeed } from "../utils/feedSlice";
 
 const Feed = () => {
-  return (
-    <div>Hello from, Feed</div>
-  )
-}
+  const feed = useSelector((state) => state.feed);
+  const dispatch = useDispatch();
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {withCredentials: true});
+      console.log(res.data);
+      
+      dispatch(addFeed(res.data));
+    } catch (error) {
+      //TODO: Handle error
+    }
+  };
 
-export default Feed
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return <div>Hello from, Feed</div>;
+};
+
+export default Feed;
