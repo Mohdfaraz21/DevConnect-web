d70b0092ff1593920361089821baaadebfbe33f4
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-02-04 14:34:54 +0530

    making usercard for the feed

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,19 +1,37 @@
 import React from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
 
-const UserCard = () => {
+
+const UserCard = ({ user }) => {
+  const { _id, firstName, lastName, photoUrl, age, gender, about } = user;
+  const dispatch = useDispatch()
+
+    const handleSendRequest = async (status, userId) => {
+      try {
+        const res = await axios.post(BASE_URL + "request/send/" + status + "/" + userId, {}, {withCredentials: true})
+        dispatch(removeUserFromFeed(userId))
+      }catch(err) {
+        console.log(err)
+      }
+    }
+
+      
+    
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-        />
+        <img src={photoUrl} alt="profile" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age && gender && <p>{age + ", " + gender}</p>}
+        <p>{about}</p>
+        <div className="card-actions justify-center my-4">
+          <button className="btn btn-primary" onClick={()=> handleSendRequest("ignored", _id)}>Ignore</button>
+          <button className="btn btn-secondary" onClick={()=> handleSendRequest("interested", _id)}>Interested</button>
         </div>
       </div>
     </div>
