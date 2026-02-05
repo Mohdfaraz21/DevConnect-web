ec4bd8eac076582e8d19226d086291439fa0b123
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-02-06 02:12:24 +0530

    fixing bugs

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,25 +4,26 @@ import axios from "axios";
 import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
 import { BASE_URL } from "../utils/constants";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
 
-  
   const fetchUser = async () => {
+    if (userData) return;
     try {
       const res = await axios.get(BASE_URL + "/profile/view", {
         withCredentials: true,
       });
       dispatch(addUser(res.data));
     } catch (err) {
-      if (err.status === 401) {
-        navigate("/login", { replace: true });
+      if (err.response && err.response.status === 401) {
+        navigate("/login");
       }
-     // console.log(err);
+      console.log(err);
     }
   };
 
